31f4561e4ac2449cd6ce21ab42a5c4041d2b97ae
Ivan Starčević <ivan.starcevic15@gmail.com>
2022-04-10 21:13:02 +0200

    End of section 3

--- DatingApp/Startup.cs
+++ DatingApp/Startup.cs
@@ -35,6 +35,7 @@ namespace DatingApp
             });
             services.AddControllers();
             services.AddSwaggerGen();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +55,8 @@ namespace DatingApp
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
